6548bf1b9f3241c2701b619ae9fbdcf3b9479506
cfgong <crystalgong123@gmail.com>
2016-03-17 21:34:12 -0500

    Update PetTester.java

--- PetTester.java
+++ PetTester.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
 public class PetTester {
-
+  public static void main(String[] args){
+		ArrayList<Pet> pets= new ArrayList();
+		// create an object of type AnimalHospital by passing it a file name (petData.txt)
+		AnimalHopsital hospital = new AnimalHospital(petData.txt);
+		//test the public methods of various classes
+}
 }
